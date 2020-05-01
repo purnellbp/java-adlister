13f52c85647600301be58d18ed4f30b0c687c7be
Brian Purnell <purnellbp@gmail.com>
2020-05-01 17:00:27 -0500

    updated dao

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -1,19 +1,64 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.User;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.regex.Pattern;
 
 @WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
 public class RegisterServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-        // TODO: show the registration form
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/users/register.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO: ensure the submitted information is valid
+        String username = request.getParameter("username");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String confirm = request.getParameter("confirm");
+
+        if (!validRegistrationInput(username, email, password)){
+            request.getRequestDispatcher("/WEB-INF/users/register.jsp").forward(request, response);
+            return;
+        }
+        if (!confirm.equals(password)){
+            request.getRequestDispatcher("/WEB-INF/users/register.jsp").forward(request, response);
+            return;
+
+        }
         // TODO: create a new user based off of the submitted information
+        User user = new User(username, email, password);
         // TODO: if a user was successfully created, send them to their profile
+        DaoFactory.getUserDao().insert(user);
+        request.getSession().setAttribute("user", user.getUsername());
+        response.sendRedirect("/profile");
+    }
+
+    private boolean validRegistrationInput(String username, String email, String password) {
+        if (!(isValidUsername(username) && isValidEmail(email) && isValidPassword(password))) return false;
+        return true;
+    }
+
+    private boolean isValidPassword(String password) {
+
+    }
+
+    private boolean isValidEmail(String email) {
+        String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+        return email.matches(regex);
+    }
+
+    private boolean isValidUsername(String username) {
+        if (username.length() <= 4 && username.length() > 20) return false;
+//        Pattern pattern = Pattern.compile("[A-Za-z_][A-Za-z0-9_]+");
+//        if(!pattern.matcher(username).matches()) return false;
+        return true;
     }
 }
--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -1,7 +1,10 @@
 package com.codeup.adlister.dao;
 
+import com.codeup.adlister.models.User;
+
 public class DaoFactory {
     private static Ads adsDao;
+    private static Users userDao;
     private static Config config = new Config();
 
     public static Ads getAdsDao() {
@@ -9,5 +12,11 @@ public class DaoFactory {
             adsDao = new MySQLAdsDao(config);
         }
         return adsDao;
+    }
+    public static Users getUserDao() {
+        if (userDao == null) {
+            userDao = new MySQLUsersDao(config);
+        }
+        return userDao;
     }
 }
